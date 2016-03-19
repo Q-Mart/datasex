847fb0774da60be2b18ca51b673537d063a7fc92
Q-Mart <qumarthjash@gmail.com>
2016-03-19 22:17:22 +0000

    WE HAVE DATA

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv
+
+fieldnames = ('Accident_Index','Location_Easting_OSGR','Location_Northing_OSGR',
+              'Longitude','Latitude','Police_Force',
+              'Accident_Severity','Number_of_Vehicles','Number_of_Casualties',
+              'Date','Day_of_Week','Time,Local_Authority_(District)','Local_Authority_(Highway)',
+              '1st_Road_Class','1st_Road_Number','Road_Type',
+              'Speed_limit','Junction_Detail','Junction_Control',
+              '2nd_Road_Class','2nd_Road_Number','Pedestrian_Crossing-Human_Control',
+              'Pedestrian_Crossing-Physical_Facilities','Light_Conditions','Weather_Conditions',
+              'Road_Surface_Conditions','Special_Conditions_at_Site','Carriageway_Hazards',
+              'Urban_or_Rural_Area','Did_Police_Officer_Attend_Scene_of_Accident','LSOA_of_Accident_Location')
+
+with open('DfTRoadSafety_Accidents_2014.csv','r') as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames)
+    for row in reader:
+        print row['Accident_Index']
