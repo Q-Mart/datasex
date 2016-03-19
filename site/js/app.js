ab1a63bf84bb59612a4d9446161d992d5f3af232
Q-Mart <qumarthjash@gmail.com>
2016-03-19 16:04:35 +0000

    Map example in d3

--- site/js/app.js
+++ site/js/app.js
@@ -8,11 +8,31 @@ $(function() {
       .attr("width", width)
       .attr("height", height);
 
+
   d3.json("/js/uk.json", function(error, uk) {
     if (error) return console.error(error);
 
+    var subunits = topojson.feature(uk, uk.objects.subunits);
+
+    var projection = d3.geo.albers()
+        .center([0, 55.4])
+        .rotate([4.4, 0])
+        .parallels([50, 60])
+        .scale(6000)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+    .projection(projection);
+
     svg.append("path")
-        .datum(topojson.feature(uk, uk.objects.subunits))
-        .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+        .datum(subunits)
+        .attr("d", path);
+
+    svg.selectAll(".subunit")
+        .data(topojson.feature(uk, uk.objects.subunits).features)
+      .enter().append("path")
+        .attr("class", function(d) { return "subunit " + d.id; })
+        .attr("d", path);
+
   });
 });
