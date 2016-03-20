c017be4129f2784f45285342a18607caf29883f9
Q-Mart <qumarthjash@gmail.com>
2016-03-20 06:12:27 +0000

    what I have

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 
+from datetime import datetime
+
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
 from bokeh.models import (
@@ -27,19 +29,41 @@ fieldnames = ('Accident_Index','Location_Easting_OSGR','Location_Northing_OSGR',
 with open('DfTRoadSafety_Accidents_2014.csv','r') as csvfile:
     reader = csv.DictReader(csvfile, fieldnames)
 
+    index=[]
     lat=[]
     lon=[]
+    no_vehicles=[]
+    no_casualties=[]
+    date=[]
+
+    lat_and_long_by_severities=[[[],[]],[[],[]],[[],[]]]
 
     reader.next()
 
     for row in reader:
+        index.append(row['Accident_Index'])
         lat.append(float(row['Latitude']))
         lon.append(float(row['Longitude']))
+        no_vehicles.append(int(row['Number_of_Vehicles']))
+        no_casualties.append(int(row['Number_of_Casualties']))
+        date.append(datetime.strptime(row['Date'], '%d/%m/%Y'))
+
+        severity = int(row['Accident_Severity']) 
+
+        if severity == 1:
+          lat_and_long_by_severities[0][0].append(float(row['Latitude']))
+          lat_and_long_by_severities[0][1].append(float(row['Longitude']))
+        elif severity == 2:
+          lat_and_long_by_severities[1][0].append(float(row['Latitude']))
+          lat_and_long_by_severities[1][1].append(float(row['Longitude']))
+        elif severity == 3:
+          lat_and_long_by_severities[2][0].append(float(row['Latitude']))
+          lat_and_long_by_severities[2][1].append(float(row['Longitude']))
 
 map_options = GMapOptions(lat=mean(lat), lng=mean(lon), map_type="roadmap", zoom=6)
 
 plot = GMapPlot(
-    x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options, title="Car Accidents in the UK during 2014"
+    x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options, title="Car Accidents in the UK during 2014", webgl=True
 )
 
 source = ColumnDataSource(
@@ -49,9 +73,36 @@ source = ColumnDataSource(
     )
 )
 
-circle = Cross(x="lon", y="lat", size=1, fill_alpha=0.8, line_color="blue")
-plot.add_glyph(source, circle)
+fatal_source = ColumnDataSource(
+    data=dict(
+        fatal_lat=lat_and_long_by_severities[0][0],
+        fatal_lon=lat_and_long_by_severities[0][1],
+    )
+)
+
+severe_source = ColumnDataSource(
+    data=dict(
+        severe_lat=lat_and_long_by_severities[1][0],
+        severe_lon=lat_and_long_by_severities[1][1],
+    )
+)
+
+slight_source = ColumnDataSource(
+    data=dict(
+        slight_lat=lat_and_long_by_severities[2][0],
+        slight_lon=lat_and_long_by_severities[2][1],
+    )
+)
+
+fatal_cross = Cross(x="fatal_lon", y="fatal_lat", size=5, fill_alpha=0.8, line_color="red")
+severe_cross = Cross(x="severe_lon", y="severe_lat", size=1, fill_alpha=0.8, line_color="green")
+slight_cross = Cross(x="slight_lon", y="slight_lat", size=1, fill_alpha=0.8, line_color="blue")
+
+plot.add_glyph(fatal_source, fatal_cross)
+# plot.add_glyph(severe_source, severe_cross)
+# plot.add_glyph(slight_source, slight_cross)
 
 plot.add_tools(PanTool(), WheelZoomTool(), BoxSelectTool())
+
 output_file("gmap_plot.html")
 show(plot)
